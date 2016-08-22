b8ef7bba55acd065d173dd9567b05428bef03e25
Emilio Molina <emolina@bmat.com>
2016-08-22 17:28:22 +0200

    Add test for smoothed pitch contour

---
+++ test_vamp_vs_libpyin/main_smooth.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <libpyincpp.h>
+#include <sndfile.h>
+#include <cstring>
+
+using namespace std;
+
+#define ARRAY_LEN(x)    ((int) (sizeof (x) / sizeof (x [0])))
+#define MAX(x,y)        ((x) > (y) ? (x) : (y))
+#define MIN(x,y)        ((x) < (y) ? (x) : (y))
+
+int main(int argc, char ** argv) {
+    SNDFILE *infile;
+    SF_INFO sfinfo;
+    float buffer[1024];
+    vector<float> samples;
+    sf_count_t count;
+    if (argc != 3) {
+        printf("\nUsage :\n\n prog <input file> <output file>\n") ;
+        exit(0);
+    }
+ 
+    memset (&sfinfo, 0, sizeof (sfinfo)) ;
+    if ((infile = sf_open (argv [1], SFM_READ, &sfinfo)) == NULL)     {     
+        printf ("Error : Not able to open input file '%s'\n", argv [1]);
+        sf_close (infile);
+        exit (1) ;
+    } 
+  
+    while ((count = sf_read_float (infile, buffer, ARRAY_LEN (buffer))) > 0) {
+        for (int i=0; i<ARRAY_LEN(buffer); i++)
+            samples.push_back(buffer[i]);
+    } 
+    sf_close (infile);
+    PyinCpp my_pyin(8000, 256, 64);
+    my_pyin.feed(samples);
+    std::vector<float> pitches = my_pyin.getSmoothedPitches();
+    for (const float pitch: pitches) {
+        cout << pitch << endl;
+    }
+    return 0;
+}
